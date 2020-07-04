b17363cd2971dbf74f763503d61ae3816a8f9ef9
Saulo Marcos Goiana <41481037+saulogoiana@users.noreply.github.com>
2020-07-04 17:17:47 -0400

    Adicionando o código
    Primeiro Commit para adicionar o código.

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.IO;
+using System.Reflection.Metadata;
+
+namespace LimpaTemp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.Title = "Limpa Temp";
+
+                string tempDirectory = "C:\\Users\\" + Environment.UserName + "\\AppData\\Local\\Temp\\";
+
+                int qtdFiles = Directory.GetFiles(tempDirectory).Length + 
+                    Directory.GetDirectories(tempDirectory).Length;
+
+                Console.WriteLine(qtdFiles + " Temporary files and folders in "+tempDirectory);
+                Console.Write("\nDelete? ( Y/ N/ Default Y): ");
+                switch (Console.ReadLine().ToUpper()) 
+                {
+                    case "N":
+                        break;
+
+                    default:
+                        try
+                        {
+                            Directory.Delete(tempDirectory,true);
+                        }
+                        catch(Exception ex)
+                        {
+                            //Console.WriteLine(ex.Message);
+                        }
+                        Console.WriteLine("Cleaned! Hope You Enjoied!");
+
+                        break;
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+
+            Console.Read();
+        }
+    }
+}
